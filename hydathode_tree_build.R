7c3ff8c4f4b2f9c7d678b9e3282251fc4b47f06b
Weston Testo <63684485+wtesto@users.noreply.github.com>
2022-05-10 11:02:33 +0200

    Updated code
    Modified character state scoring to binary (present/absent)

--- hydathode_tree_build.R
+++ hydathode_tree_build.R
@@ -15,7 +15,7 @@ name.check(tree,tipsToDrop)
 
 sampledTree<-drop.tip(tree,rownames(tipsToDrop))
 
-write.tree(sampledTree, "generaTree.tre") 
+write.tree(sampledTree, "generaTree_2.tre") 
 
 #then manually update names -- change binomials to genus names and update taxonomy
 
@@ -32,7 +32,7 @@ generaToDrop <- unknownGenera$Genus %>% as.vector()
 
 reducedTree <- drop.tip(tree, generaToDrop)
 
-write.tree(reducedTree, "treeReducea.tre")
+write.tree(reducedTree, "treeReduced.tre")
 
 
 
@@ -59,7 +59,7 @@ dev.off()
 
 
 
-### ACR for trimmed tree
+### ACR for trimmed tree (four states)
 treeReduce<-read.tree("treeReduce.tre")
 
 Y<-read.csv("hydathodeScoringReduced.csv", row.names = 1)
@@ -86,7 +86,54 @@ add.simmap.legend(leg=legendNamesReduced, colors=cols2,prompt=FALSE,x=0.9*par()$
 dev.off()
 
 
-### ACR for trimmed tree
+### ACR for trimmed tree (absent/present)
+treeReduce<-read.tree("treeReduce.tre")
+
+Y<-read.csv("hydathodeScoringReduced.csv", row.names = 1)
+
+stateReduce<-Y[treeReduce$tip.label,]
+
+stateReduceVector <- as.vector(stateReduce) #transform states to vector
+
+stateReduceVector <- gsub("sp", "ap", stateReduceVector) ##recode presence 
+
+stateReduceVector <- gsub("ev", "ap", stateReduceVector) ##recode presence
+
+names(stateReduceVector) <- treeReduce$tip.label 
+
+levels<-levels(as.factor(stateReduceVector))
+
+cols2<-setNames(c("#377eb8", "#e41a1c"), levels)
+
+legendNamesReduced<-c("Absent", "Present")
+
+fitER_Reduce_Binary <- ace(stateReduce,treeReduce, type="discrete", method = "ML")
+
+pdf(file = "Figures/Hydathode_ACR_Binary", width = 8, height = 8)
+
+plotTree(treeReduce,type="fan",fsize=0.3,ftype="i",lwd=0.5, offset = 3)
+
+tiplabels(pie=to.matrix(stateReduce,sort(unique(stateReduce))),piecol=cols2,cex=0.15)
+
+nodelabels(node=1:treeReduce$Nnode+Ntip(treeReduce),
+           pie=fitER_Reduce_Binary$lik.anc,piecol=cols2,cex=0.3)
+
+add.simmap.legend(leg=legendNamesReduced, colors=cols2,prompt=FALSE,x=0.9*par()$usr[1],
+                  y=-max(0.9*nodeHeights(treeReduce)),fsize=0.5, shape = "circle")
+dev.off()
+
+
+
+
+
+### SIMMAP for trimmed tree
+
+
+treeReduce<-read.tree("treeReduce.tre")
+
+Y<-read.csv("hydathodeScoringReduced.csv", row.names = 1)
+
+stateReduce<-Y[treeReduce$tip.label,]
 
 stateReduceVector <- as.vector(stateReduce) #transform states to vector
 
@@ -108,7 +155,7 @@ fitER <- fitMk(treeReduce, stateReduceVector, model = "ER") ##fit model for simm
 Q <- matrix(NA, length(levels),length(levels)) #make simmap transition matrix
 Q[1,]<-c(-0.005,0.005)
 Q[2,]<-c(0.005,-0.005)
-colnames(Q) <- rownames(Q) <- fitQ$states
+colnames(Q) <- rownames(Q) <- fitER$states
 
 simmapReduce <- make.simmap(treeReduce, stateReduceVector, Q = Q, nsim = 500)
 
@@ -121,6 +168,9 @@ pdf(file = "Figures/Hydathode_SimMap.pdf", width = 8, height = 8)
 
 plot(simmapReduceSummary,colors = cols2,type = "fan",
      fsize = 0.3,cex = c(0.175, 0.175), lwd= 0.25, offset = 10)
+
+nodelabels(node=1:treeReduce$Nnode+Ntip(treeReduce),
+           pie=simmapReduceSummary$ace,piecol=cols2,cex=0.3)
 
 add.simmap.legend(leg=legendNamesReduced, colors=cols2,prompt=FALSE,x=0.9*par()$usr[1],
                   y=-max(0.9*nodeHeights(treeReduce)),fsize=0.5, shape = "circle")
